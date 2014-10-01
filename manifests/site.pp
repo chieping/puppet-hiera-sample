a98cba0df19545147d7f9c93c9fe8cba384cb747
chieping <takashi.honda.01@gmail.com>
2014-10-01 20:21:35 +0900

    hiera_include

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,12 @@
 node default {
-  include sample
+  include sample1
+  hiera_include('classes')
 }
 
-class sample($message = 'sample class default message') {
+class sample1($message = 'sample1 class default message') {
+  notify { $message: }
+}
+
+class sample2($message = 'sample2 class default message') {
   notify { $message: }
 }
